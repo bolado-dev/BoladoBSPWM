41c7fa603aa0fd7fd5aeff9c2eddbb20648f747e
bolado-dev <a.bolado.dev@gmail.com>
2026-05-31 14:01:00 +0200

    fix settarget

--- Config/bin/htb_target.sh
+++ Config/bin/htb_target.sh
@@ -16,9 +16,9 @@ ip_target=$(printf '%s\n' "$line" | awk '{print $1}')
 name_target=$(printf '%s\n' "$line" | awk '{print $2}')
 
 if [ -n "$ip_target" ] && [ -n "$name_target" ]; then
-	echo "%{F#e51d0b}什%{F#ffffff} $ip_target - $name_target"
+	echo "%{F#e51d0b}ﲅ %{F#ffffff} $ip_target - $name_target"
 elif [ -n "$ip_target" ]; then
-	echo "%{F#e51d0b}什%{F#ffffff} $ip_target"
+	echo "%{F#e51d0b}ﲅ %{F#ffffff} $ip_target"
 else
 	echo "%{F#e51d0b}ﲅ %{u-}%{F#ffffff} No target"
 fi
